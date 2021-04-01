546664349597f089ce184ab52690962e336ad69c
KB0720 <81562723+KB0720@users.noreply.github.com>
2021-04-01 13:54:53 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+function openWin() {
+    myWindow=window.open("Logddd", "myWindow", "width=200, height=100");
+    myWindow.document.write("<p>succesfully cracked your divice</p>");
+  }
+
+
+
+
+
+
+
+
+
+
+function szorzas(){
+    var eredmeny = parseInt(document.getElementById("mezo1").value)*parseInt(document.getElementById("mezo2").value)*parseInt(document.getElementById("mezo3").value)
+    alert("az eredmény: " + eredmeny)
+}
+
+function plusz(){
+
+    var eredmeny = parseInt(document.getElementById("mezo1").value)+parseInt(document.getElementById("mezo2").value)+parseInt(document.getElementById("mezo3").value)
+    
+    alert("az eredmény:" + eredmeny)
+
+
+}
+
+function minus(){
+    var eredmeny = parseInt(document.getElementById("mezo1").value)-parseInt(document.getElementById("mezo2").value)-parseInt(document.getElementById("mezo3").value)
+    alert("az eredmény:" + eredmeny)
+
+
+
+}
+
+function didri(){
+    var eredmeny = parseInt(document.getElementById("mezo1").value)/parseInt(document.getElementById("mezo2").value)/parseInt(document.getElementById("mezo3").value)
+    alert("az eredmény:" + eredmeny)
+
+
+
+}
+
+
+
+
+
+function pluser(){
+
+    var eredmeny = parseInt(document.getElementById("mezo1").value)+parseInt(document.getElementById("mezo2").value)+parseInt(document.getElementById("mezo3").value)
+    
+    alert("az eredmény:" + eredmeny)
+
+
+}
